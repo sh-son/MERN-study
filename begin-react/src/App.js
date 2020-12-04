1a8ab0f3fb622ab8c94ab9c24da02a666b300a2c
sh-sh <tjdgus4722@naver.com>
2020-12-04 12:26:10 +0900

    begin-react

---
+++ begin-react/src/App.js
@@ -0,0 +1,77 @@
+import React, { useRef, useMemo, useCallback, useReducer } from "react";
+import UserList from "./component/UserList";
+import CreateUser from "./component/CreateUser";
+import produce from 'immer';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
+const initialState = {
+  inputs: {
+    username: '',
+    email: ''
+  },
+  users: [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false
+    }
+  ]
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE_USER':
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
+    case 'TOGGLE_USER':
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
+    case 'REMOVE_USER':
+      return produce(state, draft => {
+        const index = draft.user.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    default:
+      return state;
+  }
+}
+
+// UserDispatch
+export const UserDispatch = React.createContext(null);
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { users } = state;
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+  return (
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
+      <div>활성사용자 수 : {count}</div>
+    </UserDispatch.Provider>
+  );
+}
+
+export default App;
